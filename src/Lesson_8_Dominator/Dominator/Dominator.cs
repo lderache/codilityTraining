1d2cc8cd4814eb16f81c6fe2d82d10842de4b65f
laurent derache <laurent.derache@gmail.com>
2016-09-25 21:02:23 +0800

    dominator lesson

---
+++ src/Lesson_8_Dominator/Dominator/Dominator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominator
+{
+    public class Dominator
+    {
+        public int solution(int[] A)
+        {
+            if (A.Length == 0) return -1;
+
+            Dictionary<int, int> occurences = new Dictionary<int, int>();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (occurences.ContainsKey(A[i]))
+                {
+                    occurences[A[i]]++;
+                }
+                else
+                {
+                    occurences.Add(A[i], 1);
+                }
+            }
+
+            var first = occurences.OrderByDescending(x => x.Value).First();
+
+            if (first.Value > A.Length / 2)
+            {
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (A[i] == first.Key)
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/Lesson_8_Dominator/Dominator/DominatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dominator
+{
+    class DominatorTests
+    {
+        [Test]
+        public void Lesson_8_Dominator_Example()
+        {
+            var A = new int[] { 3, 4, 3, 2, 3, -1, 3, 3 };
+            var d = new Dominator();
+
+            var result = d.solution(A);
+
+            var possibleResult = new int[] { 0, 2, 4, 6, 7 };
+            Assert.IsTrue(possibleResult.Contains(result));
+        }
+
+        [Test]
+        public void Lesson_8_Dominator_Empty()
+        {
+            var A = new int[] { };
+            var d = new Dominator();
+
+            var result = d.solution(A);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Lesson_8_Dominator_Single()
+        {
+            var A = new int[] { 3 };
+            var d = new Dominator();
+
+            var result = d.solution(A);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
