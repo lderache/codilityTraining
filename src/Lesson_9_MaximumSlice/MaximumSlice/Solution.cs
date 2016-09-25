05d54899e361b18e32c3fd84fb9bc78c09f1a435
laurent derache <laurent.derache@gmail.com>
2016-09-25 13:35:36 +0800

    train on the max slice example

---
+++ src/Lesson_9_MaximumSlice/MaximumSlice/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumSlice
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            var N = A.Length;
+            List<int> pnlList = new List<int>();
+            pnlList.Add(0); // day 0
+
+            // compute pnl
+            for (int i = 0; i < N - 1; i++)
+            {
+                pnlList.Add(A[i + 1] - A[i]);
+            }
+
+            // now maximize the pnl with Kadane algorithm
+            int maxSoFar = 0;
+            int maxEndingHere = 0;
+
+            foreach (var x in pnlList)
+            {
+                maxEndingHere = Math.Max(0, maxEndingHere + x);
+                maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+            }
+
+            return maxSoFar;
+        }
+    }
+}
---
+++ src/Lesson_9_MaximumSlice/MaximumSlice/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MaximumSlice
+{
+    class Test
+    {
+        [Test]
+        public void MaximumSlice_Example()
+        {
+
+            var input = new int[] { 23171, 21011, 21123, 21366, 21013, 21367 };
+            Solution s = new Solution();
+
+            var result = s.solution(input);
+
+            // buy 1 sell 5
+            Assert.AreEqual(356, result);
+        }
+
+        [Test]
+        public void MaximumSlice_MinimumPrice()
+        {
+            var input = new int[] { 10, 11, 50, 8, 9, 10 };
+            Solution s = new Solution();
+
+            var result = s.solution(input);
+
+            // Buy 0 sell 2
+            Assert.AreEqual(40, result);
+        }
+
+        [Test]
+        public void MaximumSlice_Test()
+        {
+            var input = new int[] { 100, 99, 98, 97, 100, 98 };
+            Solution s = new Solution();
+
+            var result = s.solution(input);
+
+            // Buy 3 sell 4
+            Assert.AreEqual(3, result);
+        }
+
+    }
+}
